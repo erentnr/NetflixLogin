542af8e3d41585d24cef1c7bd39e5936bfb9c88d
erentnr <erentnr@yandex.com>
2020-08-01 16:48:54 +0300

    layout adjustments

--- app/App.js
+++ app/App.js
@@ -16,14 +16,15 @@ const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="black-content" />
-      <SafeAreaView style={{flex:1}}>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          <Login />
-          <Footer />
-        </ScrollView>
+      <SafeAreaView style={{flex:1, backgroundColor:"black"}}>
+      <ScrollView
+        contentContainerStyle={{
+          flex: 1,
+          justifyContent: 'space-between'}}>
+        <Header />
+        <Login />
+        <Footer />
+      </ScrollView>
       </SafeAreaView>
     </>
   );
@@ -32,39 +33,6 @@ const App: () => React$Node = () => {
 const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: "black",
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: "black",
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: "white",
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: "white",
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: "black",
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
   },
 });
 
